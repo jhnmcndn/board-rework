62934af57fc9f0ce139822234e55df305b572230
Christian <christian.wbridges@gmail.com>
2024-07-09 17:03:39 +0800

    chore: Add types in getAccountInfo api

--- src/api/platformApp.ts
+++ src/api/platformApp.ts
@@ -1,12 +1,11 @@
 import { APP_ROUTE } from '@/types/enums';
-import { RootResponse } from '@/types/response';
+import { GetAccountInfo, RootResponse } from '@/types/response';
 import { AxiosResponse } from 'axios';
 import { request } from './request';
 
 export const getAccountInfo = async () => {
-  const response: AxiosResponse<RootResponse<any>> = await request.post(
-    APP_ROUTE.GAME_APP + 'getAccountInfo'
-  );
+  const response: AxiosResponse<RootResponse<GetAccountInfo>> =
+    await request.post(APP_ROUTE.GAME_APP + 'getAccountInfo');
   const { data } = response.data;
   return data;
 };
