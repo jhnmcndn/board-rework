5b82459c20909eb823e8ae630f89ad1c959dfaf4
Christian <christian.wbridges@gmail.com>
2024-07-10 15:30:57 +0800

    chore: Moving getAccountInfo api in correct dir and added new api for getAccountNow

--- src/api/gameApp.ts
+++ src/api/gameApp.ts
@@ -1,11 +1,20 @@
 import { APP_ROUTE } from '@/types/enums';
-import { GetGameTypes, RootResponse } from '@/types/response';
-import { AxiosResponse } from 'axios';
+import type { GetAccountInfo, GetGameTypes, RootResponse } from '@/types/response';
+import type { AxiosResponse } from 'axios';
 import { request } from './request';
 
 export const getGameTypes = async () => {
-  const response: AxiosResponse<RootResponse<GetGameTypes>> =
-    await request.post(APP_ROUTE.GAME_APP + 'getGameTypes');
+  const response: AxiosResponse<RootResponse<GetGameTypes>> = await request.post(
+    APP_ROUTE.GAME_APP + 'getGameTypes'
+  );
+  const { data } = response.data;
+  return data;
+};
+
+export const getAccountInfo = async () => {
+  const response: AxiosResponse<RootResponse<GetAccountInfo>> = await request.post(
+    APP_ROUTE.GAME_APP + 'getAccountInfo'
+  );
   const { data } = response.data;
   return data;
 };
