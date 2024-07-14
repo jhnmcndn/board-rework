b3e351019616ba0ad5a14ed09657a86407516ada
Brix <brix.wbridges@gmail.com>
2024-07-14 13:28:58 +0800

    Implemented login api logic with machineId and ip  generation

--- src/api/platformApp.ts
+++ src/api/platformApp.ts
@@ -1,8 +1,11 @@
-import type { Init } from '@/types/app';
+import { ip } from '@/api/misc';
+import { request } from '@/api/request';
+import type { AccountInfo, Init } from '@/types/app';
 import { API_ENDPOINT, APP_ROUTE } from '@/types/enums';
+import type { LoginParams } from '@/types/params';
 import type { RootResponse } from '@/types/response';
+import { generateDeviceId } from '@/utils/app';
 import type { AxiosResponse } from 'axios';
-import { request } from './request';
 
 export const getAccountNow = async () => {
   const response: AxiosResponse<RootResponse<any>> = await request.post(
@@ -23,6 +26,25 @@ export const init = async () => {
   const response: AxiosResponse<RootResponse<Init>> = await request.post(
     APP_ROUTE.PLATFORM + API_ENDPOINT.INIT,
     params
+  );
+  const { data } = response.data;
+  return data;
+};
+
+export const login = async ({ username, password }: LoginParams) => {
+  const response: AxiosResponse<RootResponse<AccountInfo>> = await request.post(
+    APP_ROUTE.PLATFORM + API_ENDPOINT.LOGIN,
+    {
+      mobile: username,
+      passwd: password,
+      deviceId: generateDeviceId(),
+      ip: await ip(),
+    },
+    {
+      headers: {
+        version: import.meta.env.VITE_APP_VERSION,
+      },
+    }
   );
   const { data } = response.data;
   return data;
