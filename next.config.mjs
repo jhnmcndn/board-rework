d6d3a1a006d3fcfe816578c13d43f973eb3a9d03
Brix <brix.wbridges@gmail.com>
2024-07-17 13:22:38 +0800

    Added CSVGA component for svga files

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,16 @@ const nextConfig = {
     includePaths: ['./styles'],
     additionalData: `@import "./styles/_fonts.scss", "./styles/_helpers.scss", "./styles/_sizes.scss";`,
   },
+  webpack: {
+    module: {
+      rules: [
+        {
+          test: /\.svga$/i,
+          use: 'url-loader',
+        },
+      ],
+    },
+  },
 };
 
 export default nextConfig;
