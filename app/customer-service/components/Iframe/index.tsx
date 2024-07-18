9b64f740952b69d09861c6c8b48b2334dece2b94
Brix <brix.wbridges@gmail.com>
2024-07-18 16:28:28 +0800

    Added Iframe component

---
+++ app/customer-service/components/Iframe/index.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import styles from '@/app/customer-service/components/Iframe/index.module.scss';
+import { useStore } from '@/components/providers/StoreProvider';
+import { useRef } from 'react';
+
+const Iframe = () => {
+  const init = useStore((state) => state.init);
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+  const defaultCSUrl = 'https://yangxianli1.cn?groupid=bd901135e6b1fe631dd7e40e506b38d4';
+
+  const zoomContent = () => {
+    if (!iframeRef.current) return;
+    const iframeDoc = iframeRef.current.contentDocument || iframeRef.current.contentWindow?.document;
+    const iframeStyles = (
+      <style>
+        {`html, body {
+            zoom: 0.8; /* Adjust the value to zoom in or out */
+            -moz-transform: scale(0.8); /* For Firefox */
+            -webkit-transform: scale(0.8); /* For Chrome, Safari, and Opera */
+            transform: scale(0.8);
+            transform-origin: 0 0;
+          }`}
+      </style>
+    );
+    iframeDoc?.head.insertAdjacentElement('beforeend', iframeStyles as unknown as Element);
+  };
+
+  return (
+    <div className={styles.iframeContainer}>
+      <iframe
+        src={init.customerUrl || defaultCSUrl}
+        onLoad={() => zoomContent()}
+        className={styles.iframe}
+        title='Customer Service'
+      />
+    </div>
+  );
+};
+
+export default Iframe;
--- app/customer-service/components/MainContent/index.tsx
+++ app/customer-service/components/MainContent/index.tsx
@@ -1,6 +1,8 @@
 'use client';
 
+import Iframe from '@/app/customer-service/components/Iframe';
 import styles from '@/app/customer-service/components/MainContent/index.module.scss';
+import { useCSStore } from '@/components/providers/CustomerServiceProvider';
 import { useStore } from '@/components/providers/StoreProvider';
 import { CustomerServiceData } from '@/types/fns';
 import { FC } from 'react';
@@ -10,9 +12,11 @@ export type MainContentComponent = FC<Readonly<MainContentProps>>;
 
 const MainContent: MainContentComponent = ({ customerServiceData }) => {
   const theme = useStore((state) => state.theme);
+  const activeTab = useCSStore((state) => state.activeTab);
 
   return (
     <section className={styles.mainContent} data-theme={theme}>
+      {activeTab === 0 && <Iframe />}
       {/* {csActiveTab === 0 && <CsIframe />}
       {!!csPopData?.length && csActiveTab === 1 && <CsPop csPopData={csPopData} />}
       {!csPopData?.length ? csActiveTab === 1 && <FAQComponent /> : csActiveTab === 2 && <FAQComponent />} */}
