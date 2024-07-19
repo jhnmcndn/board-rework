257c02758cff7c1d344f8ac04a2ce3cf5144120a
Brix <brix.wbridges@gmail.com>
2024-07-19 16:55:07 +0800

    Added customer service pop component

--- app/customer-service/components/MainContent/index.tsx
+++ app/customer-service/components/MainContent/index.tsx
@@ -1,22 +1,32 @@
 'use client';
 
+import CSPop from '@/app/customer-service/components/CSPop';
 import Iframe from '@/app/customer-service/components/Iframe';
 import styles from '@/app/customer-service/components/MainContent/index.module.scss';
 import { useAccountStore } from '@/components/Providers/AccountStoreProvider';
 import { useCSStore } from '@/components/Providers/CSStoreProvider';
+import { CustomerService } from '@/types/app';
 import { CustomerServiceData } from '@/types/fns';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 
-export type MainContentProps = { customerServiceData: CustomerServiceData };
-export type MainContentComponent = FC<Readonly<MainContentProps>>;
+export type MainContentComponentProps = { hasCSData: boolean; csData: CustomerServiceData };
+export type MainContentComponent = FC<Readonly<MainContentComponentProps>>;
 
-const MainContent: MainContentComponent = ({ customerServiceData }) => {
+const MainContent: MainContentComponent = ({ hasCSData, csData }) => {
   const theme = useAccountStore((state) => state.theme);
   const activeTab = useCSStore((state) => state.activeTab);
+  const setCS = useCSStore((state) => state.setCS);
+
+  useEffect(() => {
+    if (hasCSData) {
+      setCS(csData as CustomerService[]);
+    }
+  }, []);
 
   return (
     <section className={styles.mainContent} data-theme={theme}>
       {activeTab === 0 && <Iframe />}
+      {hasCSData && activeTab === 1 && <CSPop />}
       {/* {csActiveTab === 0 && <CsIframe />}
       {!!csPopData?.length && csActiveTab === 1 && <CsPop csPopData={csPopData} />}
       {!csPopData?.length ? csActiveTab === 1 && <FAQComponent /> : csActiveTab === 2 && <FAQComponent />} */}
--- app/customer-service/page.tsx
+++ app/customer-service/page.tsx
@@ -5,22 +5,20 @@ import Sidebar from '@/components/Sidebar';
 import MainContent from './components/MainContent';
 
 const CustomerService = async () => {
-  const customerServiceData = await customerService();
+  const csData = await customerService();
+  const isCSDataEmpty = !csData || 'message' in csData || csData.length < 1;
 
-  const getSidebarItems = () => {
-    if (!customerServiceData) return ['在线客服', '常见问题'];
-    if ('message' in customerServiceData) return ['在线客服', '常见问题'];
-    if (customerServiceData.length < 1) return ['在线客服', '常见问题'];
+  const setSidebarItems = () => {
+    if (isCSDataEmpty) return ['在线客服', '常见问题'];
     return ['在线客服', 'POP客服', '常见问题'];
   };
 
   return (
     <main className={styles.cs}>
       <OtherHeader headerTitle='客户服务' />
-
       <div className={styles.wrapper}>
-        <Sidebar sidebarItems={getSidebarItems()} />
-        <MainContent customerServiceData={customerServiceData} />
+        <Sidebar sidebarItems={setSidebarItems()} />
+        <MainContent hasCSData={!isCSDataEmpty} csData={csData} />
       </div>
     </main>
   );
