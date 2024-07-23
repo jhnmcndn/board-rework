4fb7f751c53cd7aa296d3bbaba0dd5a0dd97dd24
Christian <christian.wbridges@gmail.com>
2024-07-23 16:14:20 +0800

    Update types for marquee

--- custom.d.ts
+++ custom.d.ts
@@ -1,9 +1,5 @@
 declare namespace JSX {
   interface IntrinsicElements {
-    marquee: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
-      behavior?: string;
-      direction?: string;
-      scrollDelay?: string;
-    };
+    marquee: React.ComponentPropsWithoutRef<'marquee'>;
   }
 }
