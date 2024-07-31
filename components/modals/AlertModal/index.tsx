2129701c2ac07a8bcc37ad514bb0dee2833bc6f5
jpaul <johnpaulsantos.wbridge@gmail.com>
2024-07-31 11:43:12 +0800

    chore: update dynamic alert icon and LoginOptionsModal sizing

--- components/modals/AlertModal/index.tsx
+++ components/modals/AlertModal/index.tsx
@@ -1,16 +1,16 @@
 'use client';
 
-import { createPortal } from 'react-dom';
-// import { serverConfig } from '@/server';
-// import Image from 'next/image';
 import ModalLayout from '@/components/modals/ModalLayout';
 import useIsMounted from '@/hooks/useIsMounted';
+import { serverConfig } from '@/server';
 import useModalStore from '@/store/modals';
+import Image from 'next/image';
+import { createPortal } from 'react-dom';
 import styles from './index.module.scss';
 
 export default function AlertContainer() {
   const { content, isAlertOpen, closeAlert } = useModalStore();
-  // const alertIcon = require(`./assets/${serverConfig.server}/favicon.ico`);
+  const alertIcon = require(`@/public/assets/${serverConfig.server}/favicon.ico`);
   const isMounted = useIsMounted();
 
   const modalContent = (
@@ -18,7 +18,7 @@ export default function AlertContainer() {
       <div className={styles.alert__wrapper}>
         <div className={styles.alert__textContainer}>
           <div className={styles.alert__imageContainer}>
-            {/* <Image src={alertIcon} alt="alert" className={styles.alert__icon} /> */}
+            <Image src={alertIcon} alt='alert' className={styles.alert__icon} />
           </div>
           {content.body && (
             <span className={styles.alert__message}>
