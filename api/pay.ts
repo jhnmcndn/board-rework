4b570a7047f5d61c607c54290194e6742bdeed68
caffeine <caffeine.wbridges@gmail.com>
2024-08-05 09:38:58 +0800

    chore: delete hard coded token in otherHeaders

--- api/pay.ts
+++ api/pay.ts
@@ -1,36 +1,36 @@
 import { request } from '@/api';
 import { BindCardList, RootResponse, BankList } from '@/types/app';
 import { API_ENDPOINT, APP_ROUTE } from '@/types/enums';
+import { getFromLocalStorage } from "@/utils/helpers";
 
 export const getBindCardList = async () => {
+  const token = getFromLocalStorage('token');
   const data = await request<RootResponse<BindCardList>>({
     route: APP_ROUTE.PAY,
     endpoint: API_ENDPOINT.BIND_CARD_LIST,
     tags: API_ENDPOINT.BIND_CARD_LIST,
     otherHeaders: {
-      // This token should be from the initial request headers.
-      // Remove after creating the auth logic!
-      token: 'Lsd61ab49381b68d7015118f3c79d947d2',
+      token: token || '',
     },
   });
   return data.data;
 };
 
 export const getBankList = async () => {
+  const token = getFromLocalStorage('token');
   const data = await request<RootResponse<BankList>>({
     route: APP_ROUTE.PAY,
     endpoint: API_ENDPOINT.BANK_LIST,
     tags: API_ENDPOINT.BANK_LIST,
     otherHeaders: {
-      // This token should be from the initial request headers.
-      // Remove after creating the auth logic!
-      token: 'Lsd61ab49381b68d7015118f3c79d947d2',
+      token: token || '',
     },
   });
   return data.data;
 }
 
 export const setBindCard = async (realName: string, bankAccount: string, bankAddress: string, bankId: number) => {
+  const token = getFromLocalStorage('token');
   const body = {
     realName,
     bankAccount,
@@ -43,12 +43,9 @@ export const setBindCard = async (realName: string, bankAccount: string, bankAdd
     tags: API_ENDPOINT.BIND_CARD,
     body,
     otherHeaders: {
-      // This token should be from the initial request headers.
-      // Remove after creating the auth logic!
-      token: 'Lsd61ab49381b68d7015118f3c79d947d2',
-      // token: localStorage.getItem('token'),
+      token: token || '',
     },
   });
-  const {code, msg} = data
-  return {code, msg};
+  const { code, msg } = data
+  return { code, msg };
 }
