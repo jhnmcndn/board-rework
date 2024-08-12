7bc0adbf6b6a43bf74347f03b64a39518c4b4f19
Brix <brix.wbridges@gmail.com>
2024-08-12 16:01:57 +0800

    Created vip details component

---
+++ app/personal-info/components/VipDetails/index.tsx
@@ -0,0 +1,36 @@
+import Table from '@/components/Table';
+import { VIPGiftInfo } from '@/types/app';
+import { FC, useMemo } from 'react';
+import styles from './index.module.scss';
+
+const VipDetails: FC<
+  Readonly<{
+    vipGiftInfo: VIPGiftInfo;
+  }>
+> = ({ vipGiftInfo }) => {
+  const headers = ['VIP级别', '总打码量范围', '晋级礼金', '周俸禄'];
+  const vipSetList = useMemo(
+    () =>
+      vipGiftInfo.vipSetList.map((setList) => ({
+        0: `VIP${setList.level}`,
+        1: `${setList.bcode.toFixed(2)}+`,
+        2: `${setList.levelBonus.toFixed(2)}元`,
+        3: `${parseFloat(setList.weekBonus.toFixed(2))}元`,
+      })),
+    [vipGiftInfo],
+  );
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.header}>
+          <span>VIP晋级模式</span>
+        </div>
+        <div className={styles.details}>
+          <Table headers={headers} content={vipSetList} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VipDetails;
---
+++ components/Table/index.tsx
@@ -0,0 +1,64 @@
+import { FC } from 'react';
+import PullToRefresh from 'react-simple-pull-to-refresh';
+import styles from './index.module.scss';
+
+const TableBody: FC<Readonly<{ content: any[]; headerLength: number }>> = ({ content, headerLength }) => {
+  return (
+    <div className={styles.body}>
+      {content.map((data, index) => (
+        <div
+          className={styles.row}
+          key={index}
+          style={{
+            display: 'grid',
+            gridTemplateColumns: `repeat(${headerLength}, 1fr)`,
+          }}
+        >
+          {Object.keys(data).map((d, idx) => (
+            <div className={styles.item} key={idx}>
+              <span>{data[d]}</span>
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const Table: FC<
+  Readonly<{
+    headers: string[];
+    content: any[];
+    withPullToRefresh?: {
+      isPullable: boolean;
+      onRefresh: () => Promise<any>;
+    };
+  }>
+> = ({ headers, content, withPullToRefresh }) => {
+  return (
+    <div className={styles.table}>
+      <div
+        className={styles.header}
+        style={{
+          display: 'grid',
+          gridTemplateColumns: `repeat(${headers.length}, 1fr)`,
+        }}
+      >
+        {headers.map((header, index) => (
+          <div key={index}>
+            <span>{header}</span>
+          </div>
+        ))}
+      </div>
+      {withPullToRefresh ? (
+        <PullToRefresh isPullable={withPullToRefresh.isPullable} onRefresh={withPullToRefresh.onRefresh}>
+          <TableBody content={content} headerLength={headers.length} />
+        </PullToRefresh>
+      ) : (
+        <TableBody content={content} headerLength={headers.length} />
+      )}
+    </div>
+  );
+};
+
+export default Table;
