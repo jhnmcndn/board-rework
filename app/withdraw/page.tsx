b9dcf2d03b214f67a77682b0b30a1522f7f26d4d
caffeine <caffeine.wbridges@gmail.com>
2024-08-13 14:04:57 +0800

    feat: initial setup for codeFlowDetails API call with types and store integration in withdraw component

--- app/withdraw/page.tsx
+++ app/withdraw/page.tsx
@@ -1,18 +1,15 @@
-'use server'
+'use server';
 
-import { WithdrawPage } from "@/app/withdraw/components/WithdrawPage";
-import { getBindCardList, getBankList } from '@/api/pay';
+import { WithdrawPage } from '@/app/withdraw/components/WithdrawPage';
+import { getBankList, getBindCardList } from '@/api/pay';
+import { getCodeFlowList } from '@/api/platform';
 
 const Withdraw = async () => {
   const bindCardList = await getBindCardList();
   const bankList = await getBankList();
+  const codeFlowList = await getCodeFlowList();
 
-  return (
-    <WithdrawPage
-      bindCardList={bindCardList}
-      bankList={bankList}
-    />
-  )
+  return <WithdrawPage bindCardList={bindCardList} bankList={bankList} codeFlowList={codeFlowList} />;
 };
 
 export default Withdraw;
