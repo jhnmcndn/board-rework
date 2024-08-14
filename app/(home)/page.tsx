67310ec0db9bfe1c10ed4c758df33bd094a2713e
feiwin <feiwin134@gmail.com>
2024-08-14 16:00:02 +0800

    feat: added app loading screen

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,7 @@
 import { getGameInfos, getGameTypes } from '@/api/game';
 import { boxPassIsOpen, getMessageHomeNotices, getMessageOnSites, init } from '@/api/platform';
 import { HomePage } from '@/app/(home)/components/';
+import LoadingScreen from '@/components/LoadingScreen';
 
 const Home = async () => {
   const initData = await init();
@@ -11,14 +12,17 @@ const Home = async () => {
   const getBoxPassIsOpen = await boxPassIsOpen();
 
   return (
-    <HomePage
-      init={initData}
-      messageHomeNoticesData={messageHomeNoticesData}
-      gameTypesData={gameTypesData}
-      gameInfosData={gameInfosData}
-      messageOnSites={messageOnSites}
-      getBoxPassIsOpen={getBoxPassIsOpen}
-    />
+    <>
+      <LoadingScreen />
+      <HomePage
+        init={initData}
+        messageHomeNoticesData={messageHomeNoticesData}
+        gameTypesData={gameTypesData}
+        gameInfosData={gameInfosData}
+        messageOnSites={messageOnSites}
+        getBoxPassIsOpen={getBoxPassIsOpen}
+      />
+    </>
   );
 };
 
---
+++ components/LoadingScreen/index.tsx
@@ -0,0 +1,69 @@
+'use client';
+import { serverConfig } from '@/server';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import styles from './index.module.scss';
+
+const LoadingScreen = () => {
+  const [showLoading, setShowLoading] = useState(false);
+  const [loadingWidth, setLoadingWidth] = useState(0);
+
+  useEffect(() => {
+    let loadingInterval: NodeJS.Timeout;
+    if (!!localStorage.getItem('loadingScreen')) {
+      setShowLoading(false);
+    } else {
+      setShowLoading(true);
+      loadingInterval = setInterval(() => {
+        setLoadingWidth((prevVal) => prevVal + 1);
+      }, 50);
+
+      if (loadingWidth === 20 || loadingWidth === 60) {
+        setLoadingWidth((prevVal) => prevVal + 20);
+      }
+
+      if (loadingWidth === 101) {
+        clearInterval(loadingInterval);
+        localStorage.setItem('loadingScreen', 'done');
+        setShowLoading(false);
+      }
+    }
+
+    return () => clearInterval(loadingInterval);
+  }, [loadingWidth]);
+
+  if (!showLoading) return null;
+
+  return (
+    <div className={styles.loadingScreen}>
+      <div className={styles.loadingScreen__backgroundImage}>
+        <Image
+          sizes='(max-width: 100vw) 100vw'
+          alt='loading_bg'
+          src={`/assets/${serverConfig.server}/loading_bg.webp`}
+          fill
+          quality={100}
+          priority
+        />
+      </div>
+      <div className={styles.loadingScreen__content}>
+        <p className={styles[`loadingScreen__text${serverConfig.server}`]}>游戏正在加载中...</p>
+        <div className={styles.loadingScreen__progressbarContainer}>
+          <div className={styles.loadingScreen__progressbarBody}>
+            <div
+              className={styles.loadingScreen__progressbar}
+              style={loadingWidth !== 0 ? { width: `${loadingWidth}%` } : {}}
+            />
+          </div>
+        </div>
+        <div className={styles.loadingScreen__version}>
+          <p className={styles[`loadingScreen__text${serverConfig.server}`]}>
+            当前版本: V {process.env.NEXT_PUBLIC_APP_VERSION}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingScreen;
