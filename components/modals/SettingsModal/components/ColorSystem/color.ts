8aaf44e5b4f9d67f4a807588c51a59623d22b481
hyper <hyper.wbridges@gmail.comgit config --global user.name hypergit config --global user.email hyper.wbridges@gmail.com>
2024-08-14 11:27:34 +0800

    chore: add color system in settings modal

---
+++ components/modals/SettingsModal/components/ColorSystem/color.ts
@@ -0,0 +1,43 @@
+import { THEME } from '@/types/enums';
+export const themeColor = [
+  {
+    theme: THEME.BLACK_GOLD,
+    color: '#FFDD8B',
+    pic: require('@/assets/commons/whiteCheck.png'),
+  },
+  {
+    theme: THEME.BROWN_GOLD,
+    color: '#875B34',
+    pic: require('@/assets/commons/whiteCheck.png'),
+  },
+  {
+    theme: THEME.YELLOW_WHITE,
+    color: '#FFC400',
+    pic: require('@/assets/commons/whiteCheck.png'),
+  },
+  {
+    theme: THEME.GREEN_YELLOW,
+    color: '#007658',
+    pic: require('@/assets/commons/whiteCheck.png'),
+  },
+  {
+    theme: THEME.SKY_BLUE,
+    color: '#0CA0E9',
+    pic: require('@/assets/commons/whiteCheck.png'),
+  },
+  {
+    theme: THEME.DARK_BLUE,
+    color: '#29468D',
+    pic: require('@/assets/commons/whiteCheck.png'),
+  },
+  {
+    theme: THEME.RED_GOLD,
+    color: '#CD071E',
+    pic: require('@/assets/commons/whiteCheck.png'),
+  },
+  {
+    theme: THEME.WHITE_GOLD,
+    color: '#FFFFFF',
+    pic: require('@/assets/commons/grayCheck.png'),
+  },
+];
--- components/modals/VersionModal/index.tsx
+++ components/modals/VersionModal/index.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useAccountStore } from '@/components/Providers/AccountStoreProvider';
 import useIsMounted from '@/hooks/useIsMounted';
 import useModalStore from '@/store/modals';
 import { AnimatePresence } from 'framer-motion';
@@ -10,13 +11,22 @@ import styles from './index.module.scss';
 
 const VersionModal: React.FC = () => {
   const { closeVersion, isVersionOpen } = useModalStore();
+  const init = useAccountStore((state) => state.init);
   const isMounted = useIsMounted();
 
   const modalContent = (
     <AnimatePresence>
-      <ModalLayout closeOnOutsideClick onClose={closeVersion}>
-        <div className={styles.versionMainContainer} style={{ border: '1px solid red', width: '100%', height: '100%' }}>
-          Display Version
+      <ModalLayout closeOnOutsideClick onClose={closeVersion} backdrop={0.5}>
+        <div className={styles.versionMainContainer}>
+          <span className={styles.notice}>有新版本，是否前往更新</span>
+          <div className={styles.buttonsContainer}>
+            <button className={styles.close} onClick={() => closeVersion()}>
+              取消
+            </button>
+            <button className={styles.redirect} onClick={() => window.open('http://' + init.webUrl, '_blank')}>
+              确定
+            </button>
+          </div>
         </div>
       </ModalLayout>
     </AnimatePresence>
