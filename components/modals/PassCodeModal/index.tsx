7cb5c3211fb1396791a078286f8d366c1d5a5ce0
caffeine <caffeine.wbridges@gmail.com>
2024-08-14 17:36:37 +0800

    feat: pass code box initial development

--- components/modals/PassCodeModal/index.tsx
+++ components/modals/PassCodeModal/index.tsx
@@ -4,26 +4,65 @@ import useModalStore from '@/store/modals';
 import { createPortal } from 'react-dom';
 import useIsMounted from '@/hooks/useIsMounted';
 import ModalLayout from '@/components/modals/ModalLayout';
-import Image from 'next/image';
+import HeaderModalTitle from '@/components/HeaderModalTitle';
 import styles from './index.module.scss';
+import Button from '@/components/Fragments/Button';
+import classnames from 'classnames';
+import { AnimatePresence } from 'framer-motion';
 
 const PassCodeModal = () => {
   const isMounted = useIsMounted();
   const { closePassCode, isPassCodeOpen } = useModalStore();
 
+  const btnValues = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    ['删除', 9, 0, '确认'],
+  ];
+
   const modalContent = (
-    <>
+    <AnimatePresence>
       {isPassCodeOpen && (
         <ModalLayout onClose={closePassCode} backdrop={0.8}>
-          <div className={styles.container}>
-            <Image src='/assets/valak.jpg' alt='valak' width={100} height={100} />
+          <div className={styles.wrapper}>
+            <HeaderModalTitle title='提现密码' onClick={closePassCode} />
+            <div className={styles.passCodeContainer}>
+              <span className={styles.title}>
+                {/*IBABALIK KO PO ITONG CONDITION WWAIT LANG*/}
+                {/*{withdrawPassIsSet ? '提现密码' : '请设置提现密码'}*/}
+                请设置提现密码
+              </span>
+
+              <div className={styles.inputWrapper}>
+                <input type='text' disabled id='first' value={'umulan'} />
+                <input type='text' disabled id='second' value={'bumagyo'} />
+                <input type='text' disabled id='third' value={'pangit'} />
+                <input type='text' disabled id='fourth' value={'ka'} />
+              </div>
+              <div className={styles.buttonsInput}>
+                {btnValues.flat().map((item) => {
+                  return (
+                    <Button
+                      id={item}
+                      key={item}
+                      className={classnames({
+                        [styles.button]: true,
+                        [styles.enterButton]: item === '确认',
+                        [styles.deleteButton]: item === '删除',
+                      })}
+                      text={item}
+                    />
+                  );
+                })}
+              </div>
+            </div>
           </div>
         </ModalLayout>
       )}
-    </>
+    </AnimatePresence>
   );
 
-  if (isMounted() && isPassCodeOpen) {
+  if (isMounted()) {
     const element = typeof window === 'undefined' ? null : (document.getElementById('modal-root') as HTMLDivElement);
     if (element) return createPortal(modalContent, element);
   }
