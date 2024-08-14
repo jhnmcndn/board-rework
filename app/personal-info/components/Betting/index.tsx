46fe16a0c5f3ea89a042dcf50a2c1bf519d3861e
Brix <brix.wbridges@gmail.com>
2024-08-14 11:52:56 +0800

    Addons

---
+++ app/personal-info/components/Betting/index.tsx
@@ -0,0 +1,15 @@
+import styles from './index.module.scss';
+
+const Betting = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.tabs}>
+          <ul></ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Betting;
