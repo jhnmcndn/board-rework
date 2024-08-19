a96a3bcd5cd37d17c8483ac7569d75cb55d96bb8
caffeine <caffeine.wbridges@gmail.com>
2024-08-19 16:14:04 +0800

    feat: recharge side bar initial development

---
+++ app/recharge/components/RechargeSidebar/index.tsx
@@ -0,0 +1,36 @@
+import { Dispatch, SetStateAction } from 'react';
+import { useAccountStore } from '@/components/Providers/AccountStoreProvider';
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import classNames from 'classnames';
+import styles from './index.module.scss';
+
+export type Props = {
+  activeSidebarItem: number;
+  setActiveSidebarItem: Dispatch<SetStateAction<number>>;
+};
+
+const RechargeSidebar = ({ setActiveSidebarItem, activeSidebarItem }: Props) => {
+  const payTypeList = useAccountStore((state) => state.payTypeList);
+  return (
+    <motion.ul initial={{ x: -100 }} animate={{ x: 0 }} transition={{ duration: 0.3 }} className={styles.listContainer}>
+      {payTypeList.map((item, index) => {
+        return (
+          <li
+            key={item.id}
+            className={classNames(styles.item, { [styles.activeItem]: item.id === activeSidebarItem })}
+            onClick={() => setActiveSidebarItem(item.id)}
+          >
+            <div className={styles.imageHolder}>
+              <Image className={styles.image} src={item.iconUrl} alt={item.name} width={200} height={200} />
+            </div>
+            <p className={styles.text}>{item.name}</p>
+            <span className={classNames(styles.recommended, { [styles.isRecommended]: item.recommend })}>热</span>
+          </li>
+        );
+      })}
+    </motion.ul>
+  );
+};
+
+export default RechargeSidebar;
