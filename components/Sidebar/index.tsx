6fa9197d1cda40f80137b60d2bfb8939ebf7099d
caffeine <caffeine.wbridges@gmail.com>
2024-08-19 16:14:49 +0800

    chore: removed unnecessary code in sidebar global

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -4,7 +4,6 @@ import styles from '@/components/Sidebar/index.module.scss';
 import { sfx } from '@/utils/audioFile';
 import classNames from 'classnames';
 import { motion } from 'framer-motion';
-import { usePathname } from 'next/navigation';
 import { Dispatch, FC, SetStateAction } from 'react';
 
 export type SidebarComponentProps = {
@@ -16,9 +15,6 @@ export type SidebarComponentProps = {
 export type SidebarComponent = FC<Readonly<SidebarComponentProps>>;
 
 const Sidebar: SidebarComponent = ({ sidebarItems, activeSidebarItem, setActiveSidebarItem }) => {
-  const pathname = usePathname();
-  const isRechargePage = pathname.toLowerCase().includes('recharge');
-
   return (
     <motion.div
       initial={{ x: -100 }}
@@ -26,56 +22,21 @@ const Sidebar: SidebarComponent = ({ sidebarItems, activeSidebarItem, setActiveS
       transition={{ duration: 0.3 }}
       className={styles.sidebarWrapper}
     >
-      {/* NOTE: The code below needs restructure!
-                Then combine with the code next to it. Does not need to be separate logic.
-      */}
-      {/* {isRechargePage && (
-        sidebarItems.map((item, index) => (
-          <div key={index} className={classNames(styles.sidebarItem, {
-            [styles.activeItem]: index === activeTab
+      {sidebarItems.map((item, index) => (
+        <div
+          key={index}
+          data-click={sfx.popAudio}
+          className={classNames(styles.sidebarItem, {
+            [styles.activeTab]: index === activeSidebarItem,
           })}
-            onClick={() => handleTabClick(index)}
-          >
-            <div className={styles.imgHolder}>
-              <Image src={item.iconUrl} alt="Icon" />
-            </div>
-
-            <div className={styles.text}>
-              <span>{item.name}</span>
-            </div>
-
-            {item.recommend && (
-              <div className={styles.recommend}>
-                <span
-                    style={{
-                      background: 'white',
-                      WebkitBackgroundClip: 'text',
-                    }}
-                  >
-                    热
-                  </span>
-              </div>
-            )}
-          </div>
-        ))
-      )} */}
-
-      {!isRechargePage &&
-        sidebarItems.map((item, index) => (
-          <div
-            key={index}
-            data-click={sfx.popAudio}
-            className={classNames(styles.sidebarItem, {
-              [styles.activeTab]: index === activeSidebarItem,
-            })}
-            bet-data={item === '' ? 'none' : undefined}
-            onClick={() => {
-              setActiveSidebarItem(index);
-            }}
-          >
-            <span className={styles.text}>{item}</span>
-          </div>
-        ))}
+          bet-data={item === '' ? 'none' : undefined}
+          onClick={() => {
+            setActiveSidebarItem(index);
+          }}
+        >
+          <span className={styles.text}>{item}</span>
+        </div>
+      ))}
     </motion.div>
   );
 };
