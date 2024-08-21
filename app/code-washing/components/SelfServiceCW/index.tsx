3d2d19bbcb52ed8695c62e2d845e9837377771b8
Arnel W. Bridges <arnel.wbrdiges@gmail.com>
2024-08-21 15:31:59 +0800

    chore: intergrate api and button redirect on rspGameTypeWashCodes list

--- app/code-washing/components/SelfServiceCW/index.tsx
+++ app/code-washing/components/SelfServiceCW/index.tsx
@@ -1,5 +1,7 @@
 import { getWashCodeDetail } from '@/api/game';
+import { useGameStore } from '@/components/Providers/GameStoreProvider';
 import { WashCodeDetail } from '@/types/app';
+import { useRouter } from 'next/navigation';
 import { useEffect, useMemo, useState } from 'react';
 import CodeWashList from './CodeWashList';
 import styles from './index.module.scss';
@@ -7,6 +9,15 @@ import TotalReceived from './TotalReceived';
 
 const SelfServiceCW = () => {
   const [washCode, setWashCode] = useState<WashCodeDetail>();
+  const sideBar = useGameStore((state) => state.sideBar);
+  const setActiveSideBarItem = useGameStore((state) => state.setActiveSideBarItem);
+  const { push } = useRouter();
+
+  const handleRedirect = (gameTypeId: number) => {
+    const item = sideBar.find((item) => item.id === gameTypeId);
+    if (item) setActiveSideBarItem(item);
+    push('/');
+  };
 
   const codeWashList = useMemo(
     () =>
@@ -15,7 +26,11 @@ const SelfServiceCW = () => {
         1: code.washCodeAmount,
         2: code.washCodeRate,
         3: code.codeAmountTotal,
-        4: <button className={styles.button}>去完成</button>,
+        4: (
+          <button className={styles.button} onClick={() => handleRedirect(code.gameTypeId)}>
+            去完成
+          </button>
+        ),
       })),
     [washCode],
   );
@@ -25,7 +40,7 @@ const SelfServiceCW = () => {
   useEffect(() => {
     const fetchWashCodeDetails = async () => {
       const codeWash = await getWashCodeDetail();
-      setWashCode(codeWash);
+      if (codeWash.rspGameTypeWashCodes) setWashCode(codeWash);
     };
     fetchWashCodeDetails();
   }, []);
