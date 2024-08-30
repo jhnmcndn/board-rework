ef19d1aaf59e28649543d1bf62b4326b51ae367f
jpaul <johnpaulsantos.wbridge@gmail.com>
2024-08-30 13:32:07 +0800

    chore: update Alert implementation

--- components/modals/ModalLayout/index.tsx
+++ components/modals/ModalLayout/index.tsx
@@ -4,27 +4,13 @@ import useClickOutside from '@/hooks/useClickOutside';
 import { ModalLayoutProps } from '@/types/app';
 import { MODAL_BG_ANIMATION, MODAL_CONTENT_ANIMATION } from '@/utils/helpers';
 import { motion } from 'framer-motion';
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 import styles from './index.module.scss';
 
-const ModalLayout = ({
-  closeOnOutsideClick = false,
-  isAlert = false,
-  children,
-  onClose,
-  backdrop = 0,
-}: ModalLayoutProps) => {
+const ModalLayout = ({ closeOnOutsideClick = false, children, onClose, backdrop = 0 }: ModalLayoutProps) => {
   const modalRef = useRef<HTMLDivElement>(null);
   const clickOutSide = useClickOutside(modalRef);
   document.body.style.overflow = 'hidden';
-
-  useEffect(() => {
-    if (isAlert) {
-      setTimeout(() => {
-        onClose?.();
-      }, 4000);
-    }
-  }, []);
 
   const handleClose = () => {
     if (closeOnOutsideClick && clickOutSide) onClose?.();
@@ -33,17 +19,17 @@ const ModalLayout = ({
 
   return (
     <motion.div
-      variants={!isAlert ? MODAL_BG_ANIMATION : {}}
+      variants={MODAL_BG_ANIMATION}
       initial='hidden'
       animate='visible'
       exit='exit'
       className={styles.modalOverlay}
       onClick={handleClose}
       id='modal-layout'
-      style={{ background: `rgba(0,0,0,${backdrop})`, ...(isAlert ? { pointerEvents: 'none' } : {}) }}
+      style={{ background: `rgba(0,0,0,${backdrop})` }}
     >
       <motion.div
-        variants={!isAlert ? MODAL_CONTENT_ANIMATION : {}}
+        variants={MODAL_CONTENT_ANIMATION}
         initial='hidden'
         animate='visible'
         exit='exit'
